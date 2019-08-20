fb73372116771ba05e66289bd35134e7538c5093
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-08-20 09:33:13 -0500

    Hola

---
+++ ForConsoleApp1/ForConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            //hia
+            Console.WriteLine("Imprime numeros del x hasta y de -1");
+            Console.WriteLine("cuando vale x?");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("cuando vale m?");
+            m = Convert.ToInt32(Console.ReadLine());
+            for (int i = n; i > m; i--)
+            {
+                Console.WriteLine("i = " + i);
+            }
+        }
+    }
+}

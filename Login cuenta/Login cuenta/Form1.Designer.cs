f2a4f4e94198ae30bb8f9e226239a6d567e6feef
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-08-20 10:35:19 -0500

    LISTO

---
+++ Login cuenta/Login cuenta/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Login_cuenta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.correo = new System.Windows.Forms.TextBox();
+            this.contrasena = new System.Windows.Forms.TextBox();
+            this.boton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(36, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Facebook";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label2.Location = new System.Drawing.Point(41, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Correo Electronico:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label3.Location = new System.Drawing.Point(41, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña:";
+            // 
+            // correo
+            // 
+            this.correo.Location = new System.Drawing.Point(234, 148);
+            this.correo.Name = "correo";
+            this.correo.Size = new System.Drawing.Size(209, 20);
+            this.correo.TabIndex = 3;
+            this.correo.TextChanged += new System.EventHandler(this.Correo_TextChanged);
+            // 
+            // contrasena
+            // 
+            this.contrasena.Location = new System.Drawing.Point(234, 212);
+            this.contrasena.Name = "contrasena";
+            this.contrasena.Size = new System.Drawing.Size(209, 20);
+            this.contrasena.TabIndex = 4;
+            this.contrasena.UseSystemPasswordChar = true;
+            // 
+            // boton
+            // 
+            this.boton.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.boton.Location = new System.Drawing.Point(44, 288);
+            this.boton.Name = "boton";
+            this.boton.Size = new System.Drawing.Size(181, 71);
+            this.boton.TabIndex = 5;
+            this.boton.Text = "Iniciar sesión";
+            this.boton.UseVisualStyleBackColor = true;
+            this.boton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.boton);
+            this.Controls.Add(this.contrasena);
+            this.Controls.Add(this.correo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cartel1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox correo;
+        private System.Windows.Forms.TextBox contrasena;
+        private System.Windows.Forms.Button boton;
+    }
+}
+
---
+++ Login cuenta/Login cuenta/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_cuenta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string co = correo.Text;
+            string cn = contrasena.Text;
+            if (co == "juancho"&& cn == "123frijoles")
+            {
+                MessageBox.Show("Accediste con exito!");
+            }
+            else
+            {
+                MessageBox.Show("tu correo o tu contraseña son incorrectos");
+            }
+        }
+
+        private void Correo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
